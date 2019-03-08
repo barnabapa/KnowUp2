4cfa5e9d08d58391f55b62dc6b6c2157bafd889a
Hubert Kołcz <kolcz.h@gmail.com>
2019-03-08 12:11:41 +0100

    add subsites, add button 'rolling to up', come corrections of txt

--- js/main.js
+++ js/main.js
@@ -1,61 +1,79 @@
+$(document).ready(function () {
+    "use strict";
 
-$(document).ready(function(){
-	"use strict";
-
-	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
-	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
-	fitscreen 			 = window_height - header_height;
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
 
 
-	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
+    $(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
 
-     
-     // -------   Active Mobile Menu-----//
 
-    $(".menu-bar").on('click', function(e){
+    // -------   Active Mobile Menu-----//
+
+    $(".menu-bar").on('click', function (e) {
         e.preventDefault();
         $("nav").toggleClass('hide');
         $("span", this).toggleClass("lnr-menu lnr-cross");
         $(".main-menu").addClass('mobile-menu');
     });
 
-    $("a").on('click', function(event) {
+    var btn = $('#button-plus');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+
+
+
+    $("a").on('click', function (event) {
 
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-    
-          // Store hash
-          var hash = this.hash;
-    
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-       
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
         } // End if
-      });
-     
+    });
+
     $('select').niceSelect();
     $('.img-pop-up').magnificPopup({
         type: 'image',
-        gallery:{
-        enabled:true
+        gallery: {
+            enabled: true
         }
     });
 
     $('.active-works-carousel').owlCarousel({
         center: true,
-        items:2,
-        loop:true,
+        items: 2,
+        loop: true,
         autoplay: true,
         margin: 100,
         dots: true,
@@ -72,21 +90,21 @@ $(document).ready(function(){
         }
     });
     // Add smooth scrolling to Menu links
-    $(".main-menu li a, .smooth").on('click', function(event) {
-            if (this.hash !== "") {
-              event.preventDefault();
-              var hash = this.hash;
-              $('html, body').animate({
+    $(".main-menu li a, .smooth").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
                 scrollTop: $(hash).offset().top - (-10)
-            }, 600, function(){
-             
+            }, 600, function () {
+
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
+    });
 
- });
+});
